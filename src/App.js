8d4b60139912f8f60d1ea40c6889bfb4aa7a22cc
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-06-12 16:15:55 +0300

    added card-list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -19,9 +20,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          this.state.monters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
-        }
+        <CardList name='Lasse'>{this.state.monters.map(monster => (
+          <h1 key={monster.id}> {monster.name} </h1>))} </CardList>
       </div>
     );
   }
