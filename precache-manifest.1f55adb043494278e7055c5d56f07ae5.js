ff60b37bf25bec1ab0adff05d251c3b32c00054b
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-06-16 15:13:10 +0300

    Updates

---
+++ precache-manifest.1f55adb043494278e7055c5d56f07ae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab8c08601d145ad26f5a1d82743eff4",
+    "url": "/CRD-2020/index.html"
+  },
+  {
+    "revision": "53476ac95bdee8400743",
+    "url": "/CRD-2020/static/css/main.e61e7b91.chunk.css"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/CRD-2020/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "53476ac95bdee8400743",
+    "url": "/CRD-2020/static/js/main.2cebd4a7.chunk.js"
+  },
+  {
+    "revision": "97ccd5f1b8f8d596ce92",
+    "url": "/CRD-2020/static/js/runtime~main.84cbd95d.js"
+  }
+]);
