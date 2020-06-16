b82c8f80d09348c0cbc8c6dcc9b1c39bbbbc2474
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-06-16 12:48:33 +0300

    added header + linear background

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ class App extends Component {
 
     return (
       <div className="App">
+      <h1> Monsters Rolotex</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={this.handleChange}
